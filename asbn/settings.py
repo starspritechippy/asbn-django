8b86d61997b7eff3501f58def9eb3e7ea430ed61
Maximilian Heymann <maximilian.heymann@telekom.de>
2023-10-05 10:48:52 +0200

    Neue Seite: wird Berrichtsheft Einträge auflisten

--- asbn/settings.py
+++ asbn/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'formular.apps.FormularConfig',
+    'liste.apps.ListeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- asbn/urls.py
+++ asbn/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('formular/', include("formular.urls"))
+    path('formular/', include("formular.urls")),
+    path('liste/', include("liste.urls"))
 ]
