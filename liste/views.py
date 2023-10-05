8eab7242d2e85e858e39b0627be5cc93e3486c47
Maximilian Heymann <maximilian.heymann@telekom.de>
2023-10-05 12:57:50 +0200

    auslesen und anzeigen von Daten aus der Datenbank

--- liste/views.py
+++ liste/views.py
@@ -1,7 +1,33 @@
-from django.http import HttpResponse
+import os
+from django.http import HttpResponse, HttpResponseServerError
 from django.shortcuts import render
 from django.template import loader
+import mariadb
 
 def index(request):
     template = loader.get_template("liste/template.html")
-    return HttpResponse(template.render({}, request))
+
+    try:
+        db_conn = mariadb.connect(
+            user = os.getenv("DB_USER"),
+            password = os.getenv("DB_PASS"),
+            host = os.getenv("DB_HOST"),
+            port = int(os.getenv("DB_PORT")),
+            database = os.getenv("DB_NAME")
+        )
+    except mariadb.Error as e:
+        return HttpResponseServerError(f"Fehler beim verbinden mit der Datenbank: {e}")
+
+    db_cursor = db_conn.cursor(dictionary=True)
+    db_cursor.execute(
+        "SELECT * FROM `asbn` ORDER BY `date` ASC, `time_start` ASC;"
+    )
+    stuff = list(db_cursor)
+
+    # step 1 Daten abfragen (nach anforderung?)
+    # step 2 daten in template einbinden
+    context = {
+        "asbn_list": stuff
+    }
+    db_conn.close()
+    return HttpResponse(template.render(context, request))
