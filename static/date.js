dc9e1fc6030e179ae3ba5957f53a7d03dc4b67a3
Maximilian Heymann <maximilian.heymann@telekom.de>
2023-10-06 08:46:28 +0200

    neuer Filter: kalenderwoche

--- static/date.js
+++ static/date.js
@@ -4,6 +4,25 @@ function todayISO() {
     return iso.substring(0, iso.indexOf('T'));
 }
 
+// Source: https://weeknumber.com/how-to/javascript
+// Returns the ISO week of the date.
+Date.prototype.getWeek = function() {
+    var date = new Date(this.getTime());
+    date.setHours(0, 0, 0, 0);
+    // Thursday in current week decides the year.
+    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+    // January 4 is always in week 1.
+    var week1 = new Date(date.getFullYear(), 0, 4);
+    // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+    return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+                          - 3 + (week1.getDay() + 6) % 7) / 7);
+  }
+
+function todayWeek() {
+    const d = new Date();
+    return d.getWeek();
+}
+
 function absenden() {
     const datum                   = document.getElementById("datum");
     const datum_missing_error     = document.getElementById("datum_missing_error");
